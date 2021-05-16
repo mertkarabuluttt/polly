cd2d91996b95bc7eedc432d32d2d5cb9f6b36ecf
mert <mertkarabulut17@icloud.com>
2021-05-16 16:51:34 +0300

    Feed component added

--- frontend/polly/src/App.js
+++ frontend/polly/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Profile from './components/Profile'
+import Feed from './components/Feed'
 
 function App() {
   return (
     <div className="App">
        <Greet/>
        <Profile/>
+       <Feed/>
     </div>
   );
 }
